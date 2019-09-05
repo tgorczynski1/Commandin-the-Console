c8a0f7e1b7084459f85c9d67baf7326333e862ae
Thomas Gorczynski <tom_8744@hotmail.com>
2019-09-05 11:37:28 -0600

    Closes #1

---
+++ src/Repo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http; // for the HttpClient (API)
+using static System.Console; // Allows me to direcly call static methonds on the Console class, without the classname qualifier 
+
+namespace Repo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World");
+        }
+    }
+}
